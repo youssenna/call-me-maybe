aed69dd7ce002a18aef15260347f0e6f5d6f6f08
Youssef Ennajar <yousenna@c3r8p7.1337.ma>
2026-06-10 12:40:36 +0100

    i finish my project

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,7 @@
 from sys import argv, exit
 from typing import List, Dict
 
-from .parser import FunctionDefinetion, Parser
+from .parser import FunctionDefinetion, Parser, FuctionCallingTest
 from pydantic import ValidationError
 import json
 from pathlib import Path
@@ -14,6 +14,31 @@ def get_name_of_valid_function(model: Small_LLM_Model,
                                available_functions: List[FunctionDefinetion],
                                reversed_vocab_dict: Dict[int, str]
                                ) -> str:
+    '''
+    this function will get the name of the valid function that I will call
+    it in the end and I will get it by sending the prompt to the model and
+    get the output and compare it with the available functions name and I
+    will do that until I get the valid function name
+
+    :param model: model to use it in encoding and getting logits from input
+        ids
+
+    :type model: Small_LLM_Model
+    :param prompt: prompt that I will send to the model to get the name of
+        valid function
+
+    :type prompt: str
+    :param available_functions: list of available functions that I will
+        compare the model output with it to get the valid function name
+
+    :type available_functions: List[FunctionDefinetion]
+    :param reversed_vocab_dict: dictionary mapping token IDs to their
+    string representations
+
+    :type reversed_vocab_dict: Dict[int, str]
+    :return: name of the valid function
+    :rtype: str
+    '''
     llm_output = ''
     available_fun_names: List[str] = \
         [func.name for func in available_functions]
@@ -35,16 +60,64 @@ def get_name_of_valid_function(model: Small_LLM_Model,
 
 
 def valid_tokens_for_boolean_param(vocab_dict: Dict[str, int]) -> List[int]:
+    '''
+    this function will return the valid tokens that I can accept in llm
+    output in case of parameter is boolean because I have only two values for
+    boolean true and false so I will return the tokens of true and false and
+    also I will return the tokens of ',' and '}' because they are the only
+    valid tokens that I can accept after the boolean value because I will
+    have only two cases after the boolean value either I will have ',' if
+    there are more parameters or I will have '}' if the boolean parameter is
+    the last parameter in the parameters list
+
+    :param vocab_dict: dictionary mapping string representations to their token
+    IDs
+
+    :type vocab_dict: Dict[str, int]
+    :return: list of valid token IDs for boolean parameters
+    :rtype: List[int]
+    '''
     return [vocab_dict['true'], vocab_dict['false'], vocab_dict[','],
             vocab_dict['}']]
 
 
 def valid_tokens_for_number_params(vocab_dict: Dict[int, str]) -> List[int]:
+    '''
+    this function will return the valid tokens that I can accept in llm
+    output in case of parameter is number because I have many values for
+    number and I will return the tokens of digits from 0 to 9 and also I
+    will return the tokens of ',' and '}' because they are the only valid
+    tokens that I can accept after the number value because I will have
+    only two cases after the number value either I will have ',' if there
+    are more parameters or I will have '}' if the number parameter is the
+    last parameter in the parameters list
+
+    :param vocab_dict: dictionary mapping token IDs to their string
+    representations
+    :type vocab_dict: Dict[int, str]
+    :return: list of valid token IDs for number parameters
+    :rtype: List[int]
+    '''
     return [k for k, v in vocab_dict.items() if v.isdigit()
             or v == '-' or v == '.' or v == ',' or v == '}']
 
 
 def find_len_of_last_digit(digits_llm_output: str, llm_output: str) -> int:
+    '''
+    this function will find the length of the last digit in the llm output
+    because I want to add ',' after the number if the number is too long to
+    prevent the model from generating very long numbers that may cause
+    overflow
+
+    :param digits_llm_output: string that contains only the digits that the
+    model generated for the number parameter
+    :type digits_llm_output: str
+    :param llm_output: the last output that the model generated for the number
+    parameter
+    :type llm_output: str
+    :return: length of the last digit in the llm output
+    :rtype: int
+    '''
     i = 0
     if len(llm_output) == 1 and llm_output.isdigit():
         for c in digits_llm_output:
@@ -61,6 +134,42 @@ def find_paramters(model: Small_LLM_Model,
                    valid_function: str,
                    reversed_vocab_dict: Dict[int, str],
                    vocab_dict: Dict[str, int]) -> str:
+    '''
+    this function will find the parameters of the valid function that I will
+    call in the end and I will get it by sending the prompt to the model and
+    get the output and compare it with the valid tokens for each parameter
+    type and I will do that until I get all the parameters of the valid
+    function and I will return the parameters in json format as a string
+    to be added in the final json object that I will save it in the output
+    file
+
+    :param model: model to use it in encoding and getting logits from input
+    ids
+    :type model: Small_LLM_Model
+
+    :param prompt: prompt that I will send to the model to get the parameters
+    of the valid function
+    :type prompt: str
+
+    :param available_functions: list of available functions that I will
+    compare the model output with it to get the valid function name and also
+    to get the parameters of the valid function
+    :type available_functions: List[FunctionDefinetion]
+
+    :param valid_function: name of the valid function that I will call in the
+    end to get its parameters
+    :type valid_function: str
+
+    :param reversed_vocab_dict: dictionary mapping token IDs to their string
+    representations
+    :type reversed_vocab_dict: Dict[int, str]
+
+    :param vocab_dict: dictionary mapping string representations to token IDs
+    :type vocab_dict: Dict[str, int]
+
+    :return: parameters of the valid function in JSON format
+    :rtype: str
+    '''
     prompt_len_at_start: int = len(prompt)
     func_object: FunctionDefinetion = list(filter(
         lambda x: x.name == valid_function, available_functions))[0]
@@ -139,6 +248,87 @@ def find_paramters(model: Small_LLM_Model,
     return prompt[prompt_len_at_start:]
 
 
+def function_calling(model: Small_LLM_Model, main_prompt: str,
+                     prompts: List[FuctionCallingTest],
+                     functions: List[FunctionDefinetion],
+                     vocab_dict_miror: Dict[int, str],
+                     vocab_dict: Dict[str, int]) -> str:
+    '''
+    this function will be responsible for the main logic of the program and it
+    will call the get_name_of_valid_function and find_parameters functions to
+    get the name of the valid function and its parameters for each prompt in
+    the prompts list and it will return the final json object that I will save
+    in the output file and it will also print the json object in a readable
+    format in the console
+
+    :param model: model to use it in encoding and getting logits from input
+    ids
+    :type model: Small_LLM_Model
+
+    :param main_prompt: main prompt that I will add the current prompt and the
+    json object that I will get from the model to it to get the name of the
+    valid function and its parameters
+    :type main_prompt: str
+
+    :param prompts: list of prompts that I will send to the model to get the
+    name of the valid function and its parameters for each prompt
+    :type prompts: List[FuctionCallingTest]
+
+    :param functions: list of available functions that I will compare the
+    model output with it to get the valid function name and also to get the
+    parameters of the valid function
+    :type functions: List[FunctionDefinetion]
+
+    :param vocab_dict_miror: dictionary mapping token IDs to their string
+    representations
+    :type vocab_dict_miror: Dict[int, str]
+
+    :param vocab_dict: dictionary mapping string representations to token IDs
+    :type vocab_dict: Dict[str, int]
+    :return: final json object that I will save it in the output file
+    :rtype: str
+    '''
+    final_json_obj: str = '['
+    i = 0
+    for prompt in prompts:
+        prompt_dict = prompt.model_dump()
+        prompt_dict['prompt'] = prompt_dict['prompt'].replace('\"', '\'')
+        print('=' * 100)
+        print('current prompt: ', end='')
+        print(prompt_dict['prompt'])
+        json_object: str = '{"prompt": "' + prompt_dict['prompt'] \
+                                          + '","name": "'
+
+        current_prompt = main_prompt + f'{prompt_dict}\n-JSON object:'\
+                                       f'\n{json_object}'
+        valid_function: str = get_name_of_valid_function(model,
+                                                         current_prompt,
+                                                         functions,
+                                                         vocab_dict_miror)
+        json_object += valid_function + '","parameters":{"'
+        current_prompt += valid_function + '","parameters":{"'
+        valid_params: str = find_paramters(
+                                            model,
+                                            current_prompt,
+                                            functions,
+                                            valid_function,
+                                            vocab_dict_miror,
+                                            vocab_dict)
+
+        json_object += valid_params
+        final_json_obj += json_object
+        if i != len(prompts) - 1:
+            final_json_obj += ','
+        print('-' * 100, '\ngoted json object I will save it later in '
+              f'output file "{argv[6]}"\n', '#' * 100, '\n')
+        j_obj = json.loads(json_object)
+        print(json.dumps(j_obj, indent=4))
+        print('\n', '#' * 100, '\n')
+        i += 1
+    final_json_obj += ']'
+    return final_json_obj
+
+
 if __name__ == '__main__':
     if not all([len(argv) == 7 or len(argv) == 9,
                 argv[1] == '--functions_definition',
@@ -185,52 +375,17 @@ if __name__ == '__main__':
             print('#'*100)
             model = Small_LLM_Model()
 
-        functions = parser.functions
-        prompts = parser.prompts
+        functions: List[FunctionDefinetion] = parser.functions
+        prompts: List[FuctionCallingTest] = parser.prompts
 
         vocab_file = model.get_path_to_vocab_file()
         with open(vocab_file, 'r') as f1, open('main_prompt.txt', 'r') as f2:
             vocab_dict = json.load(f1)
             vocab_dict_miror = {v: k for k, v in vocab_dict.items()}
             main_prompt = f2.read() + str(functions) + '\n-User request:\n'
-        final_json_obj: str = '['
-        i = 0
-        for prompt in prompts:
-            prompt_dict = prompt.model_dump()
-            prompt_dict['prompt'] = prompt_dict['prompt'].replace('\"', '\'')
-            print('=' * 100)
-            print('current prompt: ', end='')
-            print(prompt_dict['prompt'])
-            json_object: str = '{"prompt": "' + prompt_dict['prompt'] \
-                               + '","name": "'
-
-            current_prompt = main_prompt + f'{prompt_dict}\n-JSON object:'\
-                                           f'\n{json_object}'
-            valid_function: str = get_name_of_valid_function(model,
-                                                             current_prompt,
-                                                             functions,
-                                                             vocab_dict_miror)
-            json_object += valid_function + '","parameters":{"'
-            current_prompt += valid_function + '","parameters":{"'
-            valid_params: str = find_paramters(
-                                               model,
-                                               current_prompt,
-                                               functions,
-                                               valid_function,
-                                               vocab_dict_miror,
-                                               vocab_dict)
-
-            json_object += valid_params
-            final_json_obj += json_object
-            if i != len(prompts) - 1:
-                final_json_obj += ','
-            print('-' * 100, '\ngoted json object we will save it later in '
-                  f'output file "{argv[6]}"\n', '#' * 100, '\n')
-            j_obj = json.loads(json_object)
-            print(json.dumps(j_obj, indent=4))
-            print('\n', '#' * 100, '\n')
-            i += 1
-        final_json_obj += ']'
+        final_json_obj = function_calling(model, main_prompt, prompts,
+                                          functions, vocab_dict_miror,
+                                          vocab_dict)
         with open(argv[6], 'w') as f:
             obj = json.loads(final_json_obj)
             json.dump(obj, f, indent=4)
@@ -238,3 +393,6 @@ if __name__ == '__main__':
     except ValidationError as e:
         for err in e.errors():
             print(err['msg'])
+
+    except Exception as e:
+        print(str(e))
--- src/parser.py
+++ src/parser.py
@@ -6,11 +6,27 @@ from typing import List, Dict, Literal, Any, Self
 
 
 class VariableType(BaseModel):
+    '''
+    this class is responsible for representing the variable type of the
+    parameters and returns of the functions defined in the functions
+    definition file and it will have a type attribute that represent the
+    variable type of the parameters and returns of the functions defined
+    in the functions definition file and it will only accept three types
+    which are number, string and boolean
+    '''
+
     model_config = ConfigDict(extra='forbid')
     type: Literal['number', "string", 'boolean']
 
 
 class FunctionDefinetion(BaseModel):
+    '''
+    this class is responsible for representing the functions defined in the
+    functions definition file and it will have name, description, parameters
+    and returns attributes that represent the name, description, parameters
+    and returns of the function defined in the functions definition file
+    '''
+
     model_config = ConfigDict(extra='forbid')
     name: str
     description: str
@@ -19,11 +35,23 @@ class FunctionDefinetion(BaseModel):
 
 
 class FuctionCallingTest(BaseModel):
+    '''
+    this class is responsible for representing the prompts defined in the
+    prompts file and it will have a prompt attribute that represent the prompt
+    defined in the prompts file
+    '''
     model_config = ConfigDict(extra='forbid')
     prompt: str = Field(min_length=1)
 
 
 class Parser(BaseModel):
+    '''
+    this class is responsible for parsing the functions definition file and
+    the prompts file and it will store the parsed data in the functions and
+    prompts attributes of the Parser class and also it will create the output
+    file if it does not exist and it will clear the content of the output file
+    if it already exists'''
+
     model_config = ConfigDict(extra='forbid')
     functions_def_file: FilePath
     func_call_test_file: FilePath
@@ -33,6 +61,18 @@ class Parser(BaseModel):
 
     @model_validator(mode='after')
     def parse_files(self) -> Self:
+        '''
+        this function will parse the functions definition file and the prompts
+        file and it will store the parsed data in the functions and prompts
+        attributes of the Parser class and also it will create the output file
+        if it does not exist and it will clear the content of the output file
+        if it already exists
+
+        :param self: self instance of the Parser class
+        :return: self instance of the Parser class
+        :rtype: Any
+        '''
+
         path = Path(self.output_file)
         path.parent.mkdir(parents=True, exist_ok=True)
         Path(self.output_file).write_text('')
@@ -47,6 +87,24 @@ class Parser(BaseModel):
 
     def _parse_functions_def_file(self, functions_def_file: FilePath
                                   ) -> List[FunctionDefinetion]:
+        '''
+        this function will parse the functions definition file and it will
+        return a list of FunctionDefinetion objects that represent the
+        functions defined in the functions definition file and it will also
+        validate the data in the functions definition file using the
+        FunctionDefinetion model and if the data is not valid it will raise
+        a ValueError with a message 'can not proccess empty list' because we
+        can not proccess empty list of functions and also if there is any
+        error in the data it will raise a ValidationError with the error
+        message
+
+        :param self: self instance of the Parser class
+        :param functions_def_file: path to the functions definition file
+        :type functions_def_file: FilePath
+        :return: list of parsed function definitions
+        :rtype: List[FunctionDefinetion]
+        '''
+
         with open(functions_def_file, 'r') as f:
 
             data = f.read()
@@ -65,6 +123,21 @@ class Parser(BaseModel):
 
     def _parse_prompts_file(self, func_call_test_file: FilePath
                             ) -> List[FuctionCallingTest]:
+        '''
+        this method will parse the prompts file and it will return a list
+        of FuctionCallingTest objects that represent the prompts defined in
+        the prompts file and it will also validate the data in the prompts
+        file using the FuctionCallingTest model and if the data is not valid
+        it will raise a ValidationError with a message 'can not proccess empty
+        list'
+
+        :param self: self instance of the Parser class
+        :param func_call_test_file: path to the function call test file
+        :type func_call_test_file: FilePath
+        :return: list of parsed function call tests
+        :rtype: List[FuctionCallingTest]
+        '''
+
         with open(func_call_test_file, 'r') as f:
             data = f.read()
             prompt_adapter: TypeAdapter = TypeAdapter(
@@ -77,5 +150,13 @@ class Parser(BaseModel):
             return valid_prompts
 
     def __str__(self) -> str:
+        '''
+        this function will return a string representation of the Parser object
+
+        :param self: self instance of the Parser class
+        :return: string representation of the Parser object
+        :rtype: str
+        '''
+
         return f'functions = {self.functions}\n'\
             f'prompts = {self.prompts}'
